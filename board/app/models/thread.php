91af0f30795de8360c308071b9d641611fd2a886
mara <maragatchalian@github.com>
2015-02-20 18:04:42 +0800

    Added Comment Writing functions and fixed some errors regarding the links

--- board/app/models/thread.php
+++ board/app/models/thread.php
@@ -47,6 +47,16 @@ public static function get($id)
 
 	}
 
+
+ public function write(Comment $comment)                    
+   {
+        $db = DB::conn();
+        $db->query(                
+         'INSERT INTO comment SET thread_id = ?, username = ?, body = ?, created = NOW()',        
+           array($this->id, $comment->username, $comment->body)
+        );                    
+   } 
+
 }		
    
 ?>
