66fda9c0ca04c2c0b7406cfad75cc46ac7ebc2aa
mara <maragatchalian@github.com>
2015-02-20 14:45:25 +0800

    Completed thread list screen

--- board/app/views/thread/index.php
+++ board/app/views/thread/index.php
@@ -2,6 +2,12 @@
         
 <ul>
   <?php foreach ($threads as $v): ?>
-  <li><a href=""><?php eh($v->title) ?></a></li>
-  <?php endforeach ?>                    
+  <li>
+  <a href="<?php eh(url(‘thread/view’, array(‘thread_id’ => $v->id))) ?>">
+  <?php eh($v->title) ?></a>        
+  </li>
+  <?php endforeach ?>
+
+
+
 </ul>
