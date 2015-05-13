ceb0305227ed3d104a868d7b0252f2f8966b595d
mara <maragatchalian@github.com>
2015-05-13 10:44:25 +0800

    Improved Newsfeed Design

--- board/app/views/comment/home.php
+++ board/app/views/comment/home.php
@@ -3,48 +3,52 @@
 <br />
 <center>
 <font color ="black">
-<h1>NEWSFEED</h1>
+    <h1>NEWSFEED</h1>
 </center>
 
 <?php foreach ($home as $get_from_home): ?>
-  <?php foreach ($comments as $comment): ?>
-      <li>
-          <?php eh($get_from_home->username) //username ?> 
-           <br />
-          commented: <?php eh($comment->body) //comment ?> 
-          <br />
-          <?php echo time_difference($comment->created) ?> </em>  
-          <br />
-          <br />
-    </li>
-  <?php endforeach; ?> 
+    <?php foreach ($comments as $comment): ?>
+    <ul>
+        <div class ="comments">
+        <div class ="content">
+        <h5><?php eh($get_from_home->username) //username ?> </h5>
+            commented: <?php eh($comment->body) //comment ?> 
+        <br />
+        <?php echo time_difference($comment->created) ?> </em>  
+        <br />
+        <br />
+    </ul>
+    <?php endforeach; ?> 
 <?php endforeach; ?> 
 
 <?php if(empty($home)): ?>
-      No threads available.
+No threads available.
 <?php endif ?>
+
 
 <br />
 <br />
 <!--Pagination --> 
 <?php if($pagination->current > 1): ?>
-    <a class ="btn btn-small" href='?page=<?php echo($pagination->prev) ?>'>Previous</a>
-    <?php else: ?> 
-         Previous
-    <?php endif ?>
+<a class ="btn btn-small" href='?page=<?php echo($pagination->prev) ?>'>Previous</a>
+<?php else: ?> 
+Previous
+<?php endif ?>
 
 <?php for($i = 1; $i <= $pages; $i++): ?>
-    <?php if($i == $current_page): ?>
-      <?php echo $i ?>
-    <?php else: ?>
-        <a class ="btn btn-small" href='?page=<?php echo $i ?>'><?php echo $i ?></a>
-    <?php endif; ?>
+<?php if($i == $current_page): ?>
+<?php echo $i ?>
+<?php else: ?>
+<a class ="btn btn-small" href='?page=<?php echo $i ?>'><?php echo $i ?></a>
+<?php endif; ?>
 <?php endfor; ?>
 
 <?php if(!$pagination->is_last_page): ?>
-    <a class ="btn btn-small" href='?page=<?php echo $pagination->next ?>'>Next</a>
-    <?php else: ?>  Next
+<a class ="btn btn-small" href='?page=<?php echo $pagination->next ?>'>Next</a>
+<?php else: ?>  Next
 <?php endif ?>
 
 </font>
 </html>
+</div>
+</div>
